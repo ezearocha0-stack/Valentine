d0287c3895f89e5e35da5a961b5ad8654a907db7
ezearocha0-stack <ezearocha0@gmail.com>
2026-02-13 21:00:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@
 
     var textAnimate = eval(Jscex.compile("async", function () {
         var together = new Date();
-        together.setFullYear(2025, 6, 17);
+        together.setFullYear(2025, 7, 17);
         together.setHours(0);
         together.setMinutes(0);
         together.setSeconds(0);
